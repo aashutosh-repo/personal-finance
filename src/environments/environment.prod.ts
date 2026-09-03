165f03a36829406f6ac16240f7f8050de6911acd
Aashutosh Kumar <aashutoshkumar6729@gmail.com>
2026-09-03 16:52:53 +0530

    add root ai API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://spring-backend-7cps.onrender.com'
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://localhost:8080',
+  aiApiurl: 'http://localhost:8000'
 };
